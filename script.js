00a3b507c6175aec1523ce7f943fa461b7fe658f
Anton <anton20020426@gmail.com>
2023-03-30 12:18:48 +0200

    Allow to remove one book from DOM and the array

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // array where books are stored
 let myLibrary = [
-  new Book(1984, "Orwell", 250),
-  new Book("Harry Potter", "J.K.Rowling", 258),
+  new Book(1984, "Orwell", 250, 0),
+  new Book("Harry Potter", "J.K.Rowling", 258, 1),
 ];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.id = id;
 }
 
 // add new book
@@ -27,16 +28,21 @@ function addBookToLibrary() {
     return alert("Fill in all fields!");
   }
 
-  let book = new Book(bookTitle.value, bookAuthor.value, bookPages.value);
+  let book = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    bookPages.value,
+    myLibrary.length
+  );
   myLibrary.push(book);
-  createCard(book);
-  console.log(myLibrary);
+  createCard(book, myLibrary.length - 1);
   // clearInputFields();
+
+  console.log(myLibrary); // just visual help for seeing an array
 }
 
 function checkIfInputIsEmpty(field) {
   if (field.value === "") {
-    console.log("empty field");
     return true;
   }
 }
@@ -47,26 +53,21 @@ function clearInputFields() {
   bookPages.value = "";
 }
 
-// remove nodes from DOM
-const removeAll = document.querySelector(".remove-all");
-removeAll.addEventListener("click", removeFromDOM);
-function removeFromDOM() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    let child = document.querySelector(".display-titles > div");
-    console.log(`${child} ${[i]}`);
-    child.remove();
-  }
-  return (myLibrary = []);
-}
-
 // generate a book card
 const displayTitles = document.querySelector(".display-titles");
-myLibrary.forEach(createCard);
-function createCard(item) {
-  let div = document.createElement("div");
+myLibrary.forEach(createCard); // initial demonstration of manually set books on an array
+console.log(myLibrary); // just visual help for seeing an array
+function createCard(item, index) {
+  let removeOne = document.createElement("button"); // create a remove button
+  removeOne.textContent = "X";
+  removeOneBook(removeOne, index);
+
+  let div = document.createElement("div"); // create a book card
   div.classList.add("book");
+  div.setAttribute("data-index", index);
 
   for (let i = 0; i < 3; i++) {
+    // set a book object's key properties on specific paragraphs
     let para = document.createElement("p");
     if (i == 0) {
       para.textContent = `"${item.title}"`;
@@ -77,7 +78,35 @@ function createCard(item) {
     }
     div.appendChild(para);
   }
+
+  div.appendChild(removeOne);
   displayTitles.appendChild(div);
+}
+
+// remove one/all books from the library array and on the display
+const removeAll = document.querySelector(".remove-all");
+removeAll.addEventListener("click", removeFromDOM);
+function removeFromDOM() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    document.querySelector("[data-index]").remove();
+  }
+  console.log((myLibrary = [])); // just visual help for seeing an array;
+  return (myLibrary = []); // remove all books from an array too (assinging to an empty array);
+}
+
+function removeOneBook(item, position) {
+  item.addEventListener("click", () => {
+    let specificCard = document.querySelector(`[data-index="${position}"]`);
+
+    // data-attribute's value is a string
+    for (let i = 0; i < myLibrary.length; i++) {
+      if (myLibrary[i].id == specificCard.getAttribute("data-index")) {
+        myLibrary.splice([i], 1); // remove a book from an array
+        specificCard.remove(); // remove a book from DOM (on the screen)
+      }
+    }
+    console.log(myLibrary); // just visual help for seeing an array
+  });
 }
 
 // update info about books
